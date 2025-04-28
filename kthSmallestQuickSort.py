be7490cd6834184373497a23c71aaa55d1b960a7
Tony <antoniojloyola@gmail.com>
2025-04-27 21:26:46 -0700

    Implemented quickSort and used previous code from merge sort to find kth element and generating random list - TODO: Binary Search V2

--- kthSmallestQuickSort.py
+++ kthSmallestQuickSort.py
@@ -7,40 +7,49 @@ Algorithm 2: find the kth smallest element in the list using partition procedure
 Recursively Call: Recursively apply the same process to the two partitioned sub-arrays (left and right of the pivot).
 Base Case: The recursion stops when there is only one element left in the sub-array, as a single element is already sorted.
 """
-    def quickSort():
-        # Use first element as V2 will have median of medians
-        # after pick median put elements less than pivot to the left and elements great to righ
-        # left and right sides
-        # new pivot?
-        # recursivly partition left and right sub array of the already split at the end of helper func
-    def partitionList(listToPart):
-        pass
-        # use first element for partition
-        # organize elements less than to left and greater than to right 
-        #
+import random
+def quickSort(unsorted, left, right):
+    if left < right:
+        splitPoint = partitionList(unsorted, left, right)
+        
+        quickSort(unsorted, left, splitPoint-1)
+        quickSort(unsorted, splitPoint+1, right)
 
-    # create an N size list with random values
-    def randomList(size):
-        lst = []
-        for i in range(size):
-            lst.append(random.randint(i, i+20))
+def partitionList(listToPart, left, right):
+    pivot = listToPart[0] # picking first element as pivot point (non median)
 
-        return lst
+    index = left-1
 
-    def getElement(kth, arr):
-        return arr[kth-1]
+    for currI in range(left, right):
+        if listToPart[currI] <= pivot:
+            index += 1 
+            (listToPart[index], listToPart[currI]) = (listToPart[currI], listToPart[index])
+
+    (listToPart[index+1], listToPart[right]) = (listToPart[right], listToPart[index+1])
+    return index+1
+
+# create an N size list with random values
+def randomList(size):
+    lst = []
+    for i in range(size):
+        lst.append(random.randint(i, i+20))
+
+    return lst
+
+def getElement(kth, arr):
+    return arr[kth-1]
 
 
 if __name__ == "__main__":
 
     print("Random List Generated")
     arrSize = 5
-    randomList = randomList(arrSize)
-    print(randomList)
+    rList = randomList(arrSize)
+    print(rList)
 
-    sorted = quickSort(randomList)
-    k = random.randint(0, len(randomList))
-
+    quickSort(rList, 0, len(rList)-1)
+    sorted = rList
+    k = random.randint(0, len(rList))
     print("Sorted: ", sorted)
     
     print("Searhing random valid value after quickSort")
--- kthSmallestQuickSortV2.py
+++ kthSmallestQuickSortV2.py
@@ -3,3 +3,53 @@ Algorithm 3: find the kth smallest element in the list using partition procedure
 Binary Search? for medians of medians>?????
 
 """
+import random
+def quickSort(unsorted, left, right):
+    if left < right:
+        splitPoint = partitionList(unsorted, left, right)
+        
+        quickSort(unsorted, left, splitPoint-1)
+        quickSort(unsorted, splitPoint+1, right)
+
+def partitionList(listToPart, left, right):
+    pivot = len(listToPart)-1 // 2
+
+    index = left-1
+
+    for currI in range(left, right):
+        if listToPart[currI] <= pivot:
+            index += 1 
+            (listToPart[index], listToPart[currI]) = (listToPart[currI], listToPart[index])
+
+    (listToPart[index+1], listToPart[right]) = (listToPart[right], listToPart[index+1])
+    return index+1
+
+# create an N size list with random values
+def randomList(size):
+    lst = []
+    for i in range(size):
+        lst.append(random.randint(i, i+20))
+
+    return lst
+
+def getElement(kth, arr):
+    return arr[kth-1]
+
+
+if __name__ == "__main__":
+
+    print("Random List Generated")
+    arrSize = 5
+    rList = randomList(arrSize)
+    print(rList)
+
+    quickSort(rList, 0, len(rList)-1)
+    sorted = rList
+    k = random.randint(0, len(rList))
+    print("Sorted: ", sorted)
+    
+    print("Searhing random valid value after quickSort")
+    print("K value: ", k, " - Element at K: ", getElement(k, sorted))
+
+
+
